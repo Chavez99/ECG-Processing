72a4f6c98628f402c46dda16bdf834e572d2ca38
Chavez99 <64931974+Chavez99@users.noreply.github.com>
2020-10-04 16:28:44 -0700

    Add files via upload

---
+++ Com16265.m
@@ -0,0 +1,15 @@
+clear all %#ok<CLALL>
+clc
+close all
+uigetfile('16265.dat', 'Openfile.dat');
+if isequal('16265.dat', 0) || isequal('C:\Users\turok\Documents\TEC\Bioelectronics\Chavez Murga Arturo Alejandro 17491021\Matlab Signals', 0)   
+    disp('File input canceled.');  
+   ECG_Data = [];  
+else
+fid=fopen('16265.dat','r');
+end
+time = 360;
+f=(fread(fid))*(-1);
+Orig_Sig=f(1:2:length(1/f));
+plot(Orig_Sig)
+fclose(fid);
